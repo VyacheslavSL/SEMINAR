3f04e1e97a7634c752dcc3f889034e8cb21904e9
VT <yapparov.han@mail.ru>
2022-10-22 23:48:57 +0500

    add SR003

---
+++ SR003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру чила.
+
+int MaxDigit(int number)
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    int result = 0;
+    
+    if(ed > dec) result = ed;
+    if(ed < dec) result = dec;
+    return result;
+}
+
+int randNum = new Random().Next(10, 100);
+int newNum = MaxDigit(randNum);
+
+Console.WriteLine($"Max digit of {randNum} is a {newNum}");
