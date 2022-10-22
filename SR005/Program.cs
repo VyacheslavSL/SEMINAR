ae5bd3d1e57b65db7b62ae2b4df16e4f212b172a
VT <yapparov.han@mail.ru>
2022-10-23 01:51:17 +0500

    add SR005

---
+++ SR005/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход два числа выводить, является ли второе чило кратным первому. (Делится без остатка)
+bool Krat(int n1, int n2)
+{
+    if(n2 % n1 == 0 && n2 > n1)
+        return true;
+    else
+        return false;
+}
+
+Console.WriteLine("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Krat(num1, num2));
