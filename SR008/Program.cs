616ccd7e41bb5665fefe37e92cd072b279d98c68
VT <yapparov.han@mail.ru>
2022-11-08 21:04:27 +0500

    add HW_008

---
+++ SR008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+
+int FindSum(int number)
+{
+    int sum = 0;
+
+    for(int current = 1; current <= number; current++) // Величина current будет только внутри цикла!!!
+        sum += current; // +=: присваивание после сложения. Присваивает левому операнду сумму левого 
+                       // и правого операндов: выражение A += B равнозначно выражению A = A + B
+    return sum; 
+}
+
+Console.Write("Input a numder: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of number from 1 to {a} is {FindSum(a)} ");
