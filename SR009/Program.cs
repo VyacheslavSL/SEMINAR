b36041f60f675db3b3b87ebdfabcf49ebd79cb1a
VT <yapparov.han@mail.ru>
2022-11-08 22:21:00 +0500

    add HW_009

---
+++ SR009/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+
+int DigitCount(int number)
+{
+    int count = 0;
+    int num1 = number; 
+    while (num1 > 0)
+    {
+        num1 = num1 / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"cont digit is {DigitCount(a)}");
